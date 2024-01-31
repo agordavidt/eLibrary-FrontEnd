5820d891a8914a2a37b57f7fb5bd5c2ec37c88a2
David Agor <agordavid14@gmail.com>
2024-01-31 10:25:18 +0100

    hide buttons at end of scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,3 +50,38 @@ window.addEventListener("scroll", function () {
 
 });
 
+
+
+/**
+ * image slider
+ */
+
+const initSlider = () => {
+  const imageList = document.querySelector(".slider-wrapper .image-list")
+  const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+  const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+
+  // Slide images according to the slide button clicks
+
+  slideButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      /**  clientWidth returns the viewable width of an element in pixels */
+      const scrollAmount = imageList.clientWidth * direction;
+      imageList.scrollBy({ left: scrollAmount, behaviour: "smooth" });
+    });
+  });
+
+  const handleSlideButtons = () => {
+    slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none": "block";
+    slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none": "block";
+
+  }
+
+  imageList.addEventListener("scroll", () => {
+    handleSlideButtons();
+  });
+}
+
+window.addEventListener("load", initSlider);
